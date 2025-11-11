78d7c0dbcaa84749a6b2ef9276855e08b7018be7
Dolim639 <dolimbroyt.2005@gmail.com>
2025-11-11 22:46:30 +0500

    Update and rename firebase-config.js to supabase-config.js

---
+++ journal-uz/supabase-config.js
@@ -0,0 +1,4 @@
+const supabaseUrl = 'https://qwxlwterqvjsjymatpzj.supabase.co';  // Замени на свой (из Dashboard)
+const supabaseKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InF3eGx3dGVycXZqc2p5bWF0cHpqIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NjI4ODA3NzUsImV4cCI6MjA3ODQ1Njc3NX0.H-y0NaGgS431wmA8-lDiAf43-nGjCEW7SjV1y2F21RI';  // Твой anon/public key
+
+export { supabaseUrl, supabaseKey };
