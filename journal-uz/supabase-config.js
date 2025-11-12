84423b0ef9c000c97b511ba27dd197784672bb81
Dolim639 <dolimbroyt.2005@gmail.com>
2025-11-12 21:37:54 +0500

    Update supabase-config.js

--- journal-uz/supabase-config.js
+++ journal-uz/supabase-config.js
@@ -1,4 +1,5 @@
-const supabaseUrl = 'https://qwxlwterqvjsjymatpzj.supabase.co';  // Замени на свой (из Dashboard)
-const supabaseKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InF3eGx3dGVycXZqc2p5bWF0cHpqIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NjI4ODA3NzUsImV4cCI6MjA3ODQ1Njc3NX0.H-y0NaGgS431wmA8-lDiAf43-nGjCEW7SjV1y2F21RI';  // Твой anon/public key
+const supabaseUrl = 'https://qvnrgtoilopzjpybpqkm.supabase.co';  // Замени на свой (из Dashboard)
+const supabaseKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InF2bnJndG9pbG9wempweWJwcWttIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NjI5NTYyNDIsImV4cCI6MjA3ODUzMjI0Mn0.5ZVvR914LQOWxy-e9BwK6_ol3tz4GxmQiyt1CBJYjkM';  // Твой anon/public key
 
 export { supabaseUrl, supabaseKey };
+
