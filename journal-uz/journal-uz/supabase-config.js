4ccd95f4e79fff9b05524400f4280743016083cf
Dolim639 <dolimbroyt.2005@gmail.com>
2025-11-23 14:16:53 +0500

    Delete journal-uz directory

--- journal-uz/journal-uz/supabase-config.js
+++
@@ -1,5 +0,0 @@
-const supabaseUrl = 'https://qvnrgtoilopzjpybpqkm.supabase.co';  // Замени на свой (из Dashboard)
-const supabaseKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InF2bnJndG9pbG9wempweWJwcWttIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NjI5NTYyNDIsImV4cCI6MjA3ODUzMjI0Mn0.5ZVvR914LQOWxy-e9BwK6_ol3tz4GxmQiyt1CBJYjkM';  // Твой anon/public key
-
-export { supabaseUrl, supabaseKey };
-
